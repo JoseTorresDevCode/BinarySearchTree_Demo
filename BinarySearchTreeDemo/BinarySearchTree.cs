0828f18745e495afd1cb0422ff5c4fcc359bc746
JoseTorresDevCode <SgtJoseTorres@yahoo.com>
2020-08-31 00:51:43 -0500

    added more to add method

--- BinarySearchTreeDemo/BinarySearchTree.cs
+++ BinarySearchTreeDemo/BinarySearchTree.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,46 @@ namespace BinarySearchTreeDemo
 {
     class BinarySearchTree
     {
+        public Node RootNode;
+        
+
+        public void Add(Node nodeToAdd)
+        {
+            Node tempNode;
+            if (RootNode == null)
+            {
+                RootNode = nodeToAdd;
+            }
+            else if (RootNode != null)
+            {
+                Node currentNode = RootNode;
+                
+                while (nodeToAdd.Parent == null) 
+                {
+                    tempNode = currentNode;
+                    if (nodeToAdd.Data <= currentNode.Data)
+                    {
+                        currentNode = currentNode.LeftNode;
+                        if (currentNode == null)
+                        {
+                            tempNode.LeftNode = nodeToAdd;
+                            nodeToAdd.Parent = tempNode;
+                        }
+
+                    }
+                    else
+                    {
+                        currentNode = currentNode.RightNode;
+                        if (currentNode == null)
+                        {
+                            tempNode.RightNode = nodeToAdd;
+                            nodeToAdd.Parent = tempNode;
+                        }
+                    }
+                    
+                }
+            }
+            
+        }
     }
 }
--- BinarySearchTreeDemo/Program.cs
+++ BinarySearchTreeDemo/Program.cs
@@ -10,6 +10,22 @@ namespace BinarySearchTreeDemo
     {
         static void Main(string[] args)
         {
+            BinarySearchTree myList = new BinarySearchTree();
+
+            Node node1 = new Node(1);
+            Node node2 = new Node(2);
+            Node node3 = new Node(3);
+            Node node4 = new Node(4);
+            Node node5 = new Node(5);
+
+            myList.Add(node1);
+            myList.Add(node2);
+            myList.Add(node3);
+            myList.Add(node4);
+            myList.Add(node5);
+
+            BinarySearchTree add = new BinarySearchTree();
+            add.Add();
         }
     }
 }
