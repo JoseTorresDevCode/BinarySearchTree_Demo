c3235f85eb5823c9f5c5a504fc1af4da907d831b
JoseTorresDevCode <SgtJoseTorres@yahoo.com>
2020-08-31 00:24:20 -0500

    added parent node - added more to add node

--- BinarySearchTreeDemo/Node.cs
+++ BinarySearchTreeDemo/Node.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,14 @@ namespace BinarySearchTreeDemo
 {
     class Node
     {
+        public int Data;
+        public Node RightNode;
+        public Node LeftNode;
+        public Node Parent; 
+
+        public Node(int data)
+        {
+            Data = data;
+        }
     }
 }
